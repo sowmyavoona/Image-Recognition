08433636c025610231b6b44cf7609ef4de03ab9e
sowmyavoona <svoona11@gmail.com>
2016-12-26 22:59:40 +0530

    Add files via upload

--- dtw.m
+++ dtw.m
@@ -1,11 +1,11 @@
-function cost = dtw(testImage, trainImage)
- 
+function cost = dtw(testImage, trainImage, flag, titleStr )
+
     %dimOfTest = size(testImage(1,1),1); 
-    noOfVecTest = size(testImage,1);
-    
     %dimOfTrain = size(trainImage(1,1),1); 
-    noOfVecTrain = size(trainImage,1);
     
+     noOfVecTest = size(testImage,1);
+     noOfVecTrain = size(trainImage,1);
+               
     distances = zeros(noOfVecTest,noOfVecTrain);
 
     for i=1:noOfVecTest
@@ -13,22 +13,38 @@ function cost = dtw(testImage, trainImage)
              distances(i,j) = norm(testImage(i,:) - trainImage(j,:));
         end
     end
-    
-    cost = zeros(noOfVecTest,noOfVecTrain);
-    cost(1,1) = distances(1,1);
-
-    for j = 2:noOfVecTrain
-        cost(1,j) = distances(1,j) + cost(1,j-1);
-    end
-    for i = 2:noOfVecTest
-        cost(i,1) = distances(i,1) + cost(i-1,1);
-    end
-
-    for i = 2:noOfVecTest
-        for j = 2: noOfVecTrain
-            cost(i,j) = distances(i,j) + min([cost(i-1,j) cost(i,j-1) cost(i-1,j-1)]);
+   
+    for i = 1:noOfVecTest
+        for j = 1: noOfVecTrain
+            
+            if(i == 1 && j ~= 1)
+                distances(1,j) = distances(1,j) + distances(1,j-1);
+                
+            elseif(j == 1 && i ~= 1)
+                distances(i,1) = distances(i,1) + distances(i-1,1);
+                
+            elseif( i > 1 && j > 1)
+                distances(i,j) = distances(i,j) + min([distances(i-1,j) distances(i,j-1) distances(i-1,j-1)]);
+           
+            end
+            
         end
     end
+    cost = distances(noOfVecTest,noOfVecTrain)/(noOfVecTest + noOfVecTrain);
     
-    cost = cost(noOfVecTest,noOfVecTrain)/double(noOfVecTest+noOfVecTrain);
+    if( flag == 1)
+        normalCost = zeros(noOfVecTest,noOfVecTrain);
+
+        for i = 1:noOfVecTest
+            for j = 1: noOfVecTrain
+                normalCost(i,j) = distances(i,j)/double(i+j);
+            end
+        end
+        figure;
+        imagesc(normalCost);         
+        colormap(flipud(gray));
+        title(titleStr);
+    end
+    
+end
     
